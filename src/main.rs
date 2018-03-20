032e405c62152b10aa2e3f810ec506b59484ffe1
aeubanks <ayueubanks@gmail.com>
2018-03-19 22:21:52 -0700

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate lodepng;
+extern crate noise;
+extern crate rgb;
+
+use rgb::*;
+use noise::{NoiseFn, OpenSimplex};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("out.png");
+    let noise = OpenSimplex::new();
+    let buf = [255u8, 0, 0, 255, 240, 210, 123, 0, 125, 0, 0, 242];
+    lodepng::encode_file(&path, &buf, 2, 2, lodepng::ColorType::RGB, 8).unwrap();
 }
