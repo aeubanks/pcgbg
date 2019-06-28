0cad84660a9c2305820598c18b0a92cac51add6d
Arthur Eubanks <ayueubanks@gmail.com>
2019-06-27 23:23:32 -0700

    Use f64 instead of R64 wherever possible

--- src/dist.rs
+++ src/dist.rs
@@ -1,6 +1,6 @@
 use noisy_float::prelude::*;
 
-type Vec2D = euclid::Vector2D<R64>;
+type Vec2D = euclid::Vector2D<f64>;
 
 #[derive(Copy, Clone)]
 pub enum DistanceType {
@@ -11,21 +11,21 @@ pub enum DistanceType {
     MinXY,
 }
 
-pub fn distance(distance_type: DistanceType, v: Vec2D) -> R64 {
+pub fn distance(distance_type: DistanceType, v: Vec2D) -> f64 {
     let abs = v.abs();
     match distance_type {
         DistanceType::Manhattan => abs.x + abs.y,
         DistanceType::Euclidean => abs.x.powi(2) + abs.y.powi(2),
         DistanceType::Euclidean2 => (abs.x.powi(2) + abs.y.powi(2)).sqrt(),
-        DistanceType::Chebyshev => std::cmp::max(abs.x, abs.y),
-        DistanceType::MinXY => std::cmp::min(abs.x, abs.y),
+        DistanceType::Chebyshev => std::cmp::max(r64(abs.x), r64(abs.y)).raw(),
+        DistanceType::MinXY => std::cmp::min(r64(abs.x), r64(abs.y)).raw(),
     }
 }
 
 pub struct DistanceEntry {
     distance_type: DistanceType,
     size: Vec2D,
-    max_distance: R64,
+    max_distance: f64,
     center: Vec2D,
     wrap: bool,
     reverse_distance: bool,
@@ -42,7 +42,7 @@ impl DistanceEntry {
         let max_distance = distance(
             distance_type,
             if wrap {
-                size / r64(2.0)
+                size / 2.0
             } else {
                 center.max(size - center)
             },
@@ -57,7 +57,7 @@ impl DistanceEntry {
         }
     }
 
-    fn distance(&self, v: Vec2D) -> R64 {
+    fn distance(&self, v: Vec2D) -> f64 {
         let mut delta = (v - self.center).abs();
         if self.wrap {
             delta = delta.min((v - self.size - self.center).abs());
@@ -71,13 +71,13 @@ impl DistanceEntry {
         }
     }
 
-    pub fn scaled_distance(&self, v: Vec2D) -> R64 {
+    pub fn scaled_distance(&self, v: Vec2D) -> f64 {
         self.distance(v) / self.max_distance
     }
 }
 
-fn approx_eq(a: R64, b: R64) -> bool {
-    (a - b).abs() < r64(0.00000001)
+fn approx_eq(a: f64, b: f64) -> bool {
+    r64((a - b).abs()) < r64(0.00000001)
 }
 
 #[test]
@@ -85,34 +85,34 @@ fn test_dist_entry() {
     assert!(approx_eq(
         DistanceEntry::new(
             DistanceType::Manhattan,
-            Vec2D::new(r64(7.0), r64(8.0)),
-            Vec2D::new(r64(1.0), r64(0.0)),
+            Vec2D::new(7.0, 8.0),
+            Vec2D::new(1.0, 0.0),
             false,
             false
         )
-        .distance(Vec2D::new(r64(3.0), r64(3.0))),
-        r64(5.0)
+        .distance(Vec2D::new(3.0, 3.0)),
+        5.0
     ));
     assert!(approx_eq(
         DistanceEntry::new(
             DistanceType::Manhattan,
-            Vec2D::new(r64(7.0), r64(8.0)),
-            Vec2D::new(r64(1.0), r64(0.0)),
+            Vec2D::new(7.0, 8.0),
+            Vec2D::new(1.0, 0.0),
             true,
             false
         )
-        .distance(Vec2D::new(r64(6.0), r64(7.0))),
-        r64(3.0)
+        .distance(Vec2D::new(6.0, 7.0)),
+        3.0
     ));
     assert!(approx_eq(
         DistanceEntry::new(
             DistanceType::Manhattan,
-            Vec2D::new(r64(4.0), r64(2.0)),
-            Vec2D::new(r64(3.0), r64(0.0)),
+            Vec2D::new(4.0, 2.0),
+            Vec2D::new(3.0, 0.0),
             true,
             true,
         )
-        .distance(Vec2D::new(r64(0.0), r64(1.0))),
-        r64(1.0)
+        .distance(Vec2D::new(0.0, 1.0)),
+        1.0
     ));
 }
