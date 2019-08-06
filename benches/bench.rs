d9787f584f09b25599ef9ae0a3eefec5f4ee8ecb
Arthur Eubanks <ayueubanks@gmail.com>
2019-08-05 18:55:52 -0700

    Add benchmark by size from 10-100 in increments of 10

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,29 +6,37 @@ use rand::distributions::Distribution;
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
 
-fn bench(c: &mut Criterion) {
-    c.bench_function("noise and dist_entry", |b| {
-        b.iter(|| {
-            let size = black_box(100);
-            let scale = 0.5;
-            let mut rng = SmallRng::seed_from_u64(1);
+fn iter(size: usize) -> Buf {
+    let size = black_box(size);
+    let scale = 0.5;
+    let mut rng = SmallRng::seed_from_u64(1);
 
-            let noise_distribution = NoiseDistribution { scale };
-            let noise = noise_distribution.sample(&mut rng);
-            let dist_entry_distribution = DistanceEntryDistribution {
-                width: size,
-                height: size,
-            };
-            let dist_entry = dist_entry_distribution.sample(&mut rng);
+    let noise_distribution = NoiseDistribution { scale };
+    let noise = noise_distribution.sample(&mut rng);
+    let dist_entry_distribution = DistanceEntryDistribution {
+        width: size,
+        height: size,
+    };
+    let dist_entry = dist_entry_distribution.sample(&mut rng);
 
-            let mut buf = Buf::new(size, size);
-            buf.add(&noise, &[1.4, 0.1, 0.0]);
-            buf.add(&dist_entry, &[0.0, 0.1, 0.5]);
-            buf.normalize();
-            buf
-        })
-    });
+    let mut buf = Buf::new(size, size);
+    buf.add(&noise, &[1.4, 0.1, 0.0]);
+    buf.add(&dist_entry, &[0.0, 0.1, 0.5]);
+    buf.normalize();
+    buf
 }
 
-criterion_group!(benches, bench);
+fn bench(c: &mut Criterion) {
+    c.bench_function("noise and dist_entry", |b| b.iter(|| iter(100)));
+}
+
+fn inputs(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "noise and dist_entry by size",
+        |b, &&size| b.iter(|| iter(size)),
+        &[1usize, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+    );
+}
+
+criterion_group!(benches, bench, inputs);
 criterion_main!(benches);
